f71ffcdba30998adb3991d0ec027caba6163a13b
ali çavuşoğlu <alicavusoglu@alis-Mac.local>
2018-09-29 12:00:10 +0300

    initial

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import EventBus from './components/event-bus'
 
 Vue.config.productionTip = false
+
+Vue.mixin({
+  methods: {
+    getTargetNumber: function(target) {
+      setTimeout(function() { 
+        debugger;
+        EventBus.$emit("target_achieved",target)
+     }, target);
+    } 
+  }
+});
+
 
 new Vue({
   render: h => h(App)
